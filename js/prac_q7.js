d96aab90a54a8f8c44532a7c5e63b778fafbfccf
Ashasahu01 <as20252651013@gmail.com>
2026-03-02 18:48:30 +0530

    Updated prac_q6 and added prac_q7

---
+++ js/prac_q7.js
@@ -0,0 +1,7 @@
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+companies.shift();  //remover first element from given array
+
+companies.splice(2, 1, "ola");  //replace elements from given array
+
+companies.push("Amazon");
